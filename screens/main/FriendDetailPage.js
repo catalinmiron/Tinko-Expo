1f13c24442c8fbeadd7a6b85cb8557598e0e80f4
Shuaiyi Xu <305591729@qq.com>
2018-03-28 15:57:43 -0400

    删除friendListdiv

---
+++ screens/main/FriendDetailPage.js
@@ -0,0 +1,16 @@
+import React, {
+    Component
+} from 'react'
+
+import {
+    View
+} from 'react-native'
+
+export default class FriendDetailPage extends Component{
+    render() {
+        return (
+            <View>
+            </View>
+        )
+    }
+}
