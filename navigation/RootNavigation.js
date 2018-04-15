2daeafa9626a2ad9853f3cabe62f65fadf642897
Shuaiyi Xu <305591729@qq.com>
2018-04-15 17:42:41 -0400

    换成http https特别慢

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -52,7 +52,8 @@ export default class RootNavigator extends React.Component {
       this.initMeetingTable(uid);
       initNewFriendsRequestTable(uid);
 
-      this.socket = SocketIOClient('https://shuaiyixu.xyz');
+      // this.socket = SocketIOClient('https://shuaiyixu.xyz');
+      this.socket = SocketIOClient('http://47.89.187.42:4000/');
       this.socket.on("mySendBox"+uid,msg=>{
           let data = JSON.parse(msg);
           if (data.type!==999){
