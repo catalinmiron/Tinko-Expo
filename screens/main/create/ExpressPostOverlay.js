b89bf04a50c358d664946fb3d5299030d4f9221a
Donghua Xue <donghua.xue@gmail.com>
2018-05-03 01:21:04 -0400

    修改了ExpressPost获取地点和好友列表的时机

--- screens/main/create/ExpressPostOverlay.js
+++ screens/main/create/ExpressPostOverlay.js
@@ -1,9 +1,12 @@
 import React, {
     Component
 } from 'react'
-import {Text, Image, AsyncStorage, DeviceEventEmitter, Platform, Alert, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {
+    Text, Image, AsyncStorage, DeviceEventEmitter, Platform, Alert, TouchableOpacity, ActivityIndicator,
+    Dimensions
+} from 'react-native';
 import {Button, Header, Avatar, Overlay, Input} from 'react-native-elements'
-import {getMeetAvatarUri, getUserData} from "../../../modules/CommonUtility";
+import {getMeetAvatarUri, getTagName} from "../../../modules/CommonUtility";
 import {getLength,updateUnReadNum} from "../../../modules/ChatStack";
 import {createMeet, sendFriendRequest} from "../../../modules/SocketClient";
 
@@ -17,7 +20,7 @@ import {Constants, Location, Permissions, SQLite} from "expo";
 const db = SQLite.openDatabase('db.db');
 
 const allTagsList = ['#party', '#sports', '#food', '#shopping', '#movie', '#bar', '#travel', '#study', '#esports'];
-
+const SCREEN_WIDTH = Dimensions.get('window').width;
 export default class ExpressPostOverlay extends Component{
 
     static navigationOptions = {header:null};
@@ -26,7 +29,9 @@ export default class ExpressPostOverlay extends Component{
         super(props);
         props.onRef(this);
         this.showExpressPostOverlay=this.showExpressPostOverlay.bind(this);
+        let userUid = firebase.auth().currentUser.uid;
         this.state={
+            userUid:userUid,
              isVisible:false,
             placeName:'',
             placeAddress:'',
@@ -35,14 +40,7 @@ export default class ExpressPostOverlay extends Component{
             selectedFriendsList:[],
             activityIndicatorVisible:false,
         };
-        if (Platform.OS === 'android' && !Constants.isDevice) {
-            // this.setState({
-            //     errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-            // });
-        } else {
-            this.getLocationAsync();
-        }
-        this.getSql();
+
     }
 
     showExpressPostOverlay(){
@@ -56,10 +54,10 @@ export default class ExpressPostOverlay extends Component{
         }
 
         let location = await Location.getCurrentPositionAsync({});
-        this.setState({ location });
+        //this.setState({ location });
         //console.log(this.state.location);
         //console.log(this.state.location.coords.latitude);
-        fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.coords.latitude},${location.coords.longitude}&rankby=distance&key=AIzaSyCw_VwOF6hmY5yri8OpqOr9sCzTTT7JKiU`)
+        await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.coords.latitude},${location.coords.longitude}&rankby=distance&key=AIzaSyCw_VwOF6hmY5yri8OpqOr9sCzTTT7JKiU`)
             .then((response) => response.json())
             .then((responseJson) => {
                 //console.log(responseJson.results[0]);
@@ -71,27 +69,49 @@ export default class ExpressPostOverlay extends Component{
     };
 
     getSql(){
-        const { userUid } = this.state;
-        db.transaction(
-            tx => {
-                tx.executeSql('select * from friend_list'+userUid, [], (_, { rows }) => {
-                    let dataArr =  rows['_array'],
-                        rtnArr = [];
-                    for (let i = 0; i <dataArr.length;i++){
-                        rtnArr.push(dataArr[i].userId);
-                    }
-                    this.setState({ selectedFriendsList: rtnArr });
-                });
-            },
-            null,
-            this.update
-        )
+        return new Promise((resolve, reject) => {
+            const { userUid } = this.state;
+            db.transaction(
+                tx => {
+                    tx.executeSql('select * from friend_list'+userUid, [], (_, { rows }) => {
+                        let dataArr =  rows['_array'],
+                            rtnArr = [];
+                        for (let i = 0; i <dataArr.length;i++){
+                            rtnArr.push(dataArr[i].userId);
+                        }
+                        this.setState({ selectedFriendsList: rtnArr });
+                        resolve(rtnArr);
+
+                    });
+                },
+                (error) => {
+                    console.log(error);
+                    reject(error);
+                },
+                null
+            )
+        });
     }
 
 
-    createExpressMeet(tagName){
-        const{placeName, placeAddress, placeCoordinate, placeId, selectedFriendsList} = this.state;
-        let userUid = firebase.auth().currentUser.uid;
+    async createExpressMeet(tagName){
+        this.setState({activityIndicatorVisible:true});
+        await this.getLocationAsync();
+        await this.getSql()
+            .then((selectedFriendsList) => {
+                this.postMeet(tagName);
+            })
+            .catch((error) => {
+                Alert.alert('Error', error);
+            });
+
+
+    }
+
+    postMeet(tagName){
+        const{placeName, placeAddress, placeCoordinate, placeId, selectedFriendsList, userUid} = this.state;
+        console.log(this.state);
+
         let tagsCategory={};
         tagsCategory[tagName]=true;
 
@@ -124,7 +144,7 @@ export default class ExpressPostOverlay extends Component{
         });
 
         var docData = {
-            title:'Right Here Right Now!',
+            title:getTagName(tagName)+', Right Here Right Now!',
             creator: userUid,
             tagsList:[tagName],
             tagsCategory: tagsCategory,
@@ -149,58 +169,57 @@ export default class ExpressPostOverlay extends Component{
                 console.log("Document written with ID: ", meetRef.id);
                 createMeet(userUid, meetRef.id);
                 this.props.tinkoGetMeets();
+                this.setState({activityIndicator:false,isVisible:false})
             })
             .catch((error) => {
                 console.log("Error adding document: ", error);
                 Alert.alert('Error', error);
             });
-        this.setState({isVisible:false});
+
     }
 
     render() {
-        const {isVisible} = this.state;
+        const {isVisible, activityIndicatorVisible} = this.state;
         return (
-            <View>
-                <Overlay
-                    height={400}
-                    borderRadius={25}
-                    isVisible={isVisible}
-                    overlayBackgroundColor='rgba(255, 255, 255, 0.9)'
-                >
+            <Overlay
+                height={400}
+                borderRadius={25}
+                isVisible={isVisible}
+                overlayBackgroundColor='rgba(255, 255, 255, 0.9)'
+            >
 
-                    <View>
-                        {_.chunk(allTagsList, 3).map((chunk, chunkIndex) => (
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }} key={chunkIndex}>
-                                {chunk.map(tagName => (
-                                    <TouchableOpacity
-                                        onPress={() => this.createExpressMeet(tagName)}
-                                        key={tagName}
-                                        style = {{width:75}}>
-                                        <Avatar
-                                            large
-                                            rounded
-                                            source={{ uri: getMeetAvatarUri(tagName) }}
-                                            title='TK'
-                                        />
-                                        <Text
-                                            style={{marginTop:3,color:'#626567', textAlign: 'center'}}
-                                        >{tagName}</Text>
-                                    </TouchableOpacity>
+                <View>
+                    {_.chunk(allTagsList, 3).map((chunk, chunkIndex) => (
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }} key={chunkIndex}>
+                            {chunk.map(tagName => (
+                                <TouchableOpacity
+                                    onPress={() => this.createExpressMeet(tagName)}
+                                    key={tagName}
+                                    style = {{width:75}}>
+                                    <Avatar
+                                        large
+                                        rounded
+                                        source={{ uri: getMeetAvatarUri(tagName) }}
+                                        title='TK'
+                                    />
+                                    <Text
+                                        style={{marginTop:3,color:'#626567', textAlign: 'center'}}
+                                    >{tagName}</Text>
+                                </TouchableOpacity>
 
-                                ))}
-                            </View>
-                        ))}
-                        <Button
-                            containerStyle={{marginTop:15}}
-                            title='dismiss'
-                            onPress={()=> this.setState({isVisible:false})}
-                        />
-                    </View>
-                </Overlay>
-                {isVisible &&
-                <ActivityIndicator size='large' color = '#0000ff'/>
-                }
-            </View>
+                            ))}
+                        </View>
+                    ))}
+                    <Button
+                        containerStyle={{marginTop:15}}
+                        title='dismiss'
+                        onPress={()=> this.setState({isVisible:false})}
+                    />
+                    {activityIndicatorVisible &&
+                    <ActivityIndicator style={{position:'absolute', marginTop:'50%', marginLeft:SCREEN_WIDTH/2-60}} size='large' color = '#0000ff'/>
+                    }
+                </View>
+            </Overlay>
 
         )
     }
