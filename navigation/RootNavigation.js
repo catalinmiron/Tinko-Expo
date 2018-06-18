f50fb95564ba2847b886d147c331667834ba85b2
Shuaiyi Xu <305591729@qq.com>
2018-06-17 23:05:52 -0400

    修复uid没有的bug 会导致收发不了消息

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -61,8 +61,6 @@ export default class RootNavigator extends React.Component {
         // this.listener = Expo.Notifications.addListener(this.handleNotification);
         // this._notificationSubscription = this._registerForPushNotifications();
         if(this.props.loggedIn){
-            console.log("~~~~~~~~~~~~loggedIn");
-            initSocketModule(uid);
             this.loggedInSetup();
         }
     }
@@ -82,6 +80,9 @@ export default class RootNavigator extends React.Component {
       //this.dropFriendsTable(uid);
       //this.initFriendsTable(uid);
       this.initChatTable(uid);
+
+      console.log("socketModule is init for:",uid);
+      initSocketModule(uid);
 
       this.listener =DeviceEventEmitter.addListener('mySendBox',(msg)=>{
           msg = msg.msg;
